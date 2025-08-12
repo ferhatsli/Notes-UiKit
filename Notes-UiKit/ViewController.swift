ac84be7bcdd24fc89d88c7d74178d90df8b5b0a8
ferhatsli <evoo3363@gmail.com>
2025-08-12 11:22:31 +0300

    Initial Commit

---
+++ Notes-UiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes-UiKit
+//
+//  Created by Ferhat Taşlı on 12.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
